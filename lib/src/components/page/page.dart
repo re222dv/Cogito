cd40ba77213e27c74dc7baa8180eb0f4d0f951bc
Rasmus Eneman <re222dv@student.lnu.se>
2014-04-07 12:07:20 +0200

    Add basic support for rendering paths

--- lib/src/components/page/page.dart
+++ lib/src/components/page/page.dart
@@ -8,11 +8,13 @@ part of cogito;
 )
 class PageComponent {
     Page page;
-    
+
     PageComponent() {
         page = new Page();
-        
-        page.nodes.add(new Text()..color='red'..text='Hello, World!'..x=50..y=50..size=20);
-        page.nodes.add(new Text()..color='green'..text='Test'..x=160..y=160..size=50);
+
+        page.texts.add(new Text()..color='red'..text='Hello, World!'..x=50..y=50..size=20);
+        page.texts.add(new Text()..color='green'..text='Test'..x=160..y=160..size=50);
+
+        page.paths.add(new Path()..color='blue'..path='M 55 22 L 272 99'..width=20..x=50..y=50);
     }
 }
--- lib/src/model.dart
+++ lib/src/model.dart
@@ -1,12 +1,19 @@
 part of cogito;
 
 class Page {
-    List<Node> nodes = new List<Node>();
+    List<Path> paths = new List<Path>();
+    List<Text> texts = new List<Text>();
 }
 
 abstract class Node {
     int x;
     int y;
+}
+
+class Path extends Node {
+    String color;
+    String path;
+    int width;
 }
 
 class Text extends Node {
