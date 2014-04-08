7a64cf4949030ce728c83dd2021edb0cf3ebf5f7
Rasmus Eneman <re222dv@student.lnu.se>
2014-04-08 16:55:20 +0200

    Fix problem when moving objects too fast

--- lib/src/directives/node-handler.dart
+++ lib/src/directives/node-handler.dart
@@ -7,24 +7,25 @@ class NodeHandlerDirective {
     @NgOneWay('node-handler')
     Node node;
 
-    bool moving = false;
-    int offsetX;
-    int offsetY;
-
     NodeHandlerDirective(this.element) {
         element.onMouseDown.listen((MouseEvent e) {
-            offsetX = node.x - e.offset.x;
-            offsetY = node.y - e.offset.y;
-            moving = true;
+            var offsetX = node.x - e.offset.x;
+            var offsetY = node.y - e.offset.y;
+
+            var sub = element.parent.onMouseMove.listen((MouseEvent e) {
+                node.x = e.offset.x + offsetX;
+                node.y = e.offset.y + offsetY;
+
+                e.preventDefault();
+                e.stopPropagation();
+            });
+
+            element.parent.onMouseUp.first.then((_) {
+                sub.cancel();
+            });
+
             e.preventDefault();
             e.stopPropagation();
         });
-        element.onMouseMove.where((_) => moving).listen((MouseEvent e) {
-            node.x = e.offset.x + offsetX;
-            node.y = e.offset.y + offsetY;
-            e.preventDefault();
-            e.stopPropagation();
-        });
-        element.onMouseUp.listen((_) => moving = false);
     }
 }
