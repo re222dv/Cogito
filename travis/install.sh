c15127d5e0b922abd96c801a4be193a352b3ce58
Rasmus Eneman <re222dv@student.lnu.se>
2014-05-11 22:00:56 +0200

    (Hopefully) Fix content_shell on travis

--- travis/install.sh
+++ travis/install.sh
@@ -4,7 +4,7 @@ set -exv
 
 sh -e /etc/init.d/xvfb start
 sudo apt-get update -qq
-echo "msttcorefonts msttcorefonts/accepted-mscorefonts-eula boolean true" | debconf-set-selections
+echo "msttcorefonts msttcorefonts/accepted-mscorefonts-eula boolean true" | sudo debconf-set-selections
 sudo apt-get install -qq unzip apache2 wdiff php5-cgi ttf-indic-fonts \
    msttcorefonts ttf-dejavu-core ttf-kochi-gothic ttf-kochi-mincho \
    ttf-thai-tlwg chromium-browser
