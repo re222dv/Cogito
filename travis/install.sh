85ec85c1a635662ef1e1888d67123ed467179661
Rasmus Eneman <re222dv@student.lnu.se>
2014-05-11 21:43:21 +0200

    (Hopefully) Fix content_shell on travis

--- travis/install.sh
+++ travis/install.sh
@@ -4,5 +4,7 @@ set -exv
 
 sh -e /etc/init.d/xvfb start
 sudo apt-get update -qq
-sudo apt-get install -qq unzip ttf-kochi-gothic ttf-kochi-mincho
+sudo apt-get install -qq unzip apache2 wdiff php5-cgi ttf-indic-fonts \
+   msttcorefonts ttf-dejavu-core ttf-kochi-gothic ttf-kochi-mincho \
+   ttf-thai-tlwg
 sudo apt-get install libxss1
